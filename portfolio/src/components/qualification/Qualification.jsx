d79ba48a7e924d75742043cfd4170b792d802acf
SrestMilan <shrestham780@gmail.com>
2024-01-02 09:42:30 +0545

    qualification added

--- portfolio/src/components/qualification/Qualification.jsx
+++ portfolio/src/components/qualification/Qualification.jsx
@@ -1,12 +1,121 @@
-import React from "react";
+import React, { useState } from "react";
 import "./qualification.css";
 
 const Qualification = () => {
+  const [toggle, setToggle] = useState(1);
+  const toggleTab = (index) => {
+    setToggle(index);
+  };
   return (
     <section className="qualification section">
       <h2 className="section__title">Qualification</h2>
 
       <span className="section__subtitle">My personel journey</span>
+      <div className="qualification__container container">
+        <div className="qualification__tabs">
+          <div
+            className={
+              toggle === 1
+                ? "qualification__button qualification__active button--flex"
+                : "qualification__button button--flex"
+            }
+            onClick={() => toggleTab(1)}
+          >
+            <i className="uil uil-graduation-cap qualification__icon">
+              Education
+            </i>
+          </div>
+          <div
+            className={
+              toggle === 2
+                ? "qualification__button qualification__active button--flex"
+                : "qualification__button button--flex"
+            }
+            onClick={() => toggleTab(2)}
+          >
+            <i className="uil uil-briefcase-alt qualification__icon">
+              Experience
+            </i>
+          </div>
+        </div>
+        <div className="qualification__sections">
+          <div
+            className={
+              toggle === 1
+                ? "qualification__content qualification__content-active"
+                : "qualification__content "
+            }
+          >
+            <div className="qualification__data">
+              <div>
+                <h3 className="qualification__title">Frontend Engineer</h3>
+                <span className="qualification__subtitle">
+                  Herald College Kathmandu
+                </span>
+                <div className="qualification__calendar">
+                  <i className="uil uil-calendar-alt"></i>2021 - 2023
+                </div>
+              </div>
+              <div>
+                <span className="qualification__rounder"></span>
+                <span className="qualification__line"></span>
+              </div>
+            </div>
+            <div className="qualification__data">
+              <div></div>
+              <div>
+                <span className="qualification__rounder"></span>
+                <span className="qualification__line"></span>
+              </div>
+              <div>
+                <h3 className="qualification__title">Student</h3>
+                <span className="qualification__subtitle">
+                  St. Lawrence Secondary School
+                </span>
+                <div className="qualification__calendar">
+                  <i className="uil uil-calendar-alt"></i>2018 - 2020
+                </div>
+              </div>
+            </div>
+            <div className="qualification__data">
+              <div>
+                <h3 className="qualification__title">Student</h3>
+                <span className="qualification__subtitle">
+                  East Pole Higher Secondary School
+                </span>
+                <div className="qualification__calendar">
+                  <i className="uil uil-calendar-alt"></i>2008 - 2018
+                </div>
+              </div>
+              <div>
+                <span className="qualification__rounder"></span>
+                <span className="qualification__line"></span>
+              </div>
+            </div>
+          </div>
+          <div
+            className={
+              toggle === 2
+                ? "qualification__content qualification__content-active"
+                : "qualification__content "
+            }
+          >
+            <div className="qualification__data">
+              <div>
+                <h3 className="qualification__title">Web Master</h3>
+                <span className="qualification__subtitle">Foreveryng</span>
+                <div className="qualification__calendar">
+                  <i className="uil uil-calendar-alt"></i>Aug 2023- Sept 2023
+                </div>
+              </div>
+              <div>
+                <span className="qualification__rounder"></span>
+                <span className="qualification__line"></span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </section>
   );
 };
